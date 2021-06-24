6f95fc19e72b3cc89d709e0303c90334f0371067
tandica <williams.tandica@gmail.com>
2021-06-24 22:57:45 +0000

    add user login, register and authentication functionality and style the pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+  get "/register" => "users#new"
+  get "/users" => "users#create"
   get "/about", to: "about#index"
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
